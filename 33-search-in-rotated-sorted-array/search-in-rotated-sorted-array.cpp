0ececc1761842d85415a4806db2b1a0a06034152
Partho Paul <10legendgaming@gmail.com>
2026-02-24 01:02:53 +0600

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.78%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if(find(nums.begin(),nums.end(),target)!=nums.end())
+        auto i=find(nums.begin(),nums.end(),target);
+        if(i!=nums.end())
         {
-            return (find(nums.begin(),nums.end(),target)-nums.begin());
+            return i-nums.begin();
         }
         else
         {
